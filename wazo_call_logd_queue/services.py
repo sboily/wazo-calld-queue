dceb5a32ac450d22c919f59b24239c90b6577e3d
Sylvain Boily <sylvainboilydroid@gmail.com>
2021-10-27 11:55:18 -0400

    insert queue_log data into db to dont break wazo stats

--- wazo_call_logd_queue/services.py
+++ wazo_call_logd_queue/services.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import logging
-
+from xivo_dao.alchemy.queue_log import QueueLog
 
 class Services(object):
 
@@ -13,5 +13,20 @@ class Services(object):
 
     def queue_log(self, queue_log):
         self._publisher._queue_log(queue_log)
-        #self._dao.queue_log.insert_entry(**queue_log)
+        with self._dao.cel.new_session() as session:
+            self.insert_entry(session, queue_log)
         return 1
+
+    def insert_entry(self, session, queue_log):
+        entry = QueueLog(
+            time=queue_log['time'],
+            callid=queue_log['callid'],
+            queuename=queue_log['queuename'],
+            agent=queue_log['agent'],
+            event=queue_log['event'],
+            data1=queue_log['data1'],
+            data2=queue_log['data2'],
+            data3=queue_log['data3'],
+            data4=queue_log['data4'],
+            data5=queue_log['data5'])
+        session.add(entry)
