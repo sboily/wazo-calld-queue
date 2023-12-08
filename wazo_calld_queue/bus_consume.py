82b276d5fe6ec88edd79ac4e63344315f3bd5aee
Mathias WOLFF <mathias@celea.org>
2023-12-08 17:23:42 +0100

    queue stats v1

--- wazo_calld_queue/bus_consume.py
+++ wazo_calld_queue/bus_consume.py
@@ -4,6 +4,7 @@
 import logging, math
 
 from .events import (
+    AgentConnectEvent,
     QueueCallerAbandonEvent,
     QueueCallerJoinEvent,
     QueueCallerLeaveEvent,
@@ -19,6 +20,10 @@ from .events import (
 
 stats = {}
 
+# stats = [
+# {'name', 'count', 'received', 'abandonned', 'answered', 'awr'}
+# ]
+
 MY_TENANT = '6209d5e0-4015-4853-ab2b-2e556bef5e46'
 
 logger = logging.getLogger(__name__)
@@ -30,6 +35,7 @@ class QueuesBusEventHandler(object):
         self.bus_publisher = bus_publisher
 
     def subscribe(self, bus_consumer):
+        bus_consumer.subscribe('AgentConnect', self._agent_connect)
         bus_consumer.subscribe('QueueCallerAbandon', self._queue_caller_abandon)
         bus_consumer.subscribe('QueueCallerJoin', self._queue_caller_join)
         bus_consumer.subscribe('QueueCallerLeave', self._queue_caller_leave)
@@ -39,6 +45,15 @@ class QueuesBusEventHandler(object):
         bus_consumer.subscribe('QueueMemberRemoved', self._queue_member_removed)
         bus_consumer.subscribe('QueueMemberRinginuse', self._queue_member_ringinuse)
         bus_consumer.subscribe('QueueMemberStatus', self._queue_member_status)
+
+    def _agent_connect(self, event):
+        #print(event)
+        tenant_uuid = self._extract_tenant_uuid(event)
+        bus_event = AgentConnectEvent(
+            event,
+            tenant_uuid
+        )
+        self.bus_publisher.publish(bus_event)
 
     def _queue_caller_abandon(self, event):
         tenant_uuid = self._extract_tenant_uuid(event)
@@ -124,12 +139,12 @@ class QueuesBusEventHandler(object):
             event,
             tenant_uuid
         )
+        print(bus_event)
         self.bus_publisher.publish(bus_event)
 
     def _livestats(self, event, tenant_uuid):
-        
-
         name = event['Queue']
+
         # If the queue stats doesnot exist, create the object with default values
         if not stats.get(name):
             stats.update({
@@ -140,39 +155,40 @@ class QueuesBusEventHandler(object):
                     'abandonned': 0,
                     'answered': 0,
                     'awr': 0,
-                    'waiting_calls': {}
+                    'waiting_calls': []
                 }
             })
         
         queue_event = event['Event']
         if queue_event == "QueueCallerJoin":
             stats[name]['count'] = int(event['Count'])
-            stats[name]['waiting_calls'].update({
-                event['Uniqueid']: {
-                    'calleridnum': event['CallerIDNum'],
-                    'calleridname': event['CallerIDName'],
-                    'position': event['Position'],
-                    'channelstate': event['ChannelState'],
-                    'channelstatedesc': event['ChannelStateDesc'],
-                    'time': event['ChanVariable']['WAZO_ANSWER_TIME'],
-                    'entryexten': event['ChanVariable']['WAZO_ENTRY_EXTEN']
-                }
+            stats[name]['waiting_calls'].append({
+                'uniqueid': event['Uniqueid'],
+                'calleridnum': event['CallerIDNum'],
+                'calleridname': event['CallerIDName'],
+                'position': event['Position'],
+                'channelstate': event['ChannelState'],
+                'channelstatedesc': event['ChannelStateDesc'],
+                'time': event['ChanVariable']['WAZO_ANSWER_TIME'],
+                'entryexten': event['ChanVariable']['WAZO_ENTRY_EXTEN']
             })
         elif queue_event == "QueueCallerAbandon":
             stats[name]['abandonned'] += 1
             stats[name]['answered'] -= 1
             if stats[name]['received'] > 0:
                 stats[name]['awr'] = math.ceil(stats[name]['answered'] / stats[name]['received'] * 100)
-            if stats[name]['waiting_calls'].get(event['Uniqueid']):
-                stats[name]['waiting_calls'].pop(event['Uniqueid'])
+            for i in range(len(stats[name]['waiting_calls'])):
+                if stats[name]['waiting_calls'][i]['uniqueid'] == event['Uniqueid']:
+                    stats[name]['waiting_calls'].pop(i)
         elif queue_event == "QueueCallerLeave":
             stats[name]['count'] = int(event['Count'])
             stats[name]['answered'] += 1
             stats[name]['received'] += 1
             if stats[name]['received'] > 0:
                 stats[name]['awr'] = math.ceil(stats[name]['answered'] / stats[name]['received'] * 100)
-            if stats[name]['waiting_calls'].get(event['Uniqueid']):
-                stats[name]['waiting_calls'].pop(event['Uniqueid'])
+            for i in range(len(stats[name]['waiting_calls'])):
+                if stats[name]['waiting_calls'][i]['uniqueid'] == event['Uniqueid']:
+                    stats[name]['waiting_calls'].pop(i)
 
         #Set color depending on limit value
         stats[name]['count_color'] = "green";
